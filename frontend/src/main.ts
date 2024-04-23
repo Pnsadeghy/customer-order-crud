8b34ae38fa195ce0bbbab48e29cc17bdd5d76744
Pouria <pnsadeghy@gmail.com>
2024-04-23 19:18:26 +0330

    Feat: Install formkit

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,3 +1,4 @@
+import { plugin, defaultConfig } from '@formkit/vue'
 import { createPinia } from 'pinia'
 import { createApp } from 'vue'
 import "@/assets/main.scss"
@@ -6,7 +7,7 @@ import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
-
+app.use(plugin, defaultConfig)
 app.use(createPinia())
 app.use(router)
 
