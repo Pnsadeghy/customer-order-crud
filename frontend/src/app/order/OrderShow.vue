a0c77897a7f08beef97968552592aa304897d91f
Pouria <pnsadeghy@gmail.com>
2024-04-24 03:23:05 +0330

    Feat: Vue order print basic

--- frontend/src/app/order/OrderShow.vue
+++ frontend/src/app/order/OrderShow.vue
@@ -1,43 +1,51 @@
 <template>
   <app-card class="p-4" >
       <app-page-header :title="`#${order.code}`">
-          <app-button
-                  shape="default icon" size="small" theme="outline-secondary"
-                  v-if="editMode" @click.prevent="editMode = false">
-              <ChevronLeftIcon />
-          </app-button>
-          <template v-else >
-              <app-button @click.prevent="editMode = true" shape="default icon" size="small" >
+          <template v-if="tab === TabEnum.Detail" >
+              <app-button @click.prevent="tab = TabEnum.Edit" shape="default icon" size="small" >
                   <PencilIcon />
               </app-button>
-              <app-button shape="default icon" size="small" theme="outline-secondary" >
+              <app-button @click.prevent="tab = TabEnum.Print" shape="default icon" size="small" theme="outline-secondary" >
                   <PrinterIcon />
               </app-button>
           </template>
+          <app-button
+              shape="default icon" size="small" theme="outline-secondary"
+              v-else @click.prevent="tab = TabEnum.Detail">
+              <ChevronLeftIcon />
+          </app-button>
       </app-page-header>
 
-      <order-form v-if="editMode"
+
+      <order-detail v-if="tab === TabEnum.Detail" :order="order" />
+      <order-print v-if="tab === TabEnum.Print" :order="order" />
+      <order-form v-if="tab === TabEnum.Edit"
                   @success="onUpdated"
                   :item="order"
                   :api="updateApi" />
-      <order-detail v-else :order="order" />
   </app-card>
 </template>
 
 <script setup lang="ts">
 import {updateNotification} from "@/components/notification/utils/notification.utils"
+import {PencilIcon, PrinterIcon, ChevronLeftIcon} from "@heroicons/vue/24/outline"
 import type OrderInterface from "@/app/order/interfaces/order.interface"
 import AppPageHeader from "@/components/page-header/AppPageHeader.vue"
-import {PencilIcon, PrinterIcon} from "@heroicons/vue/24/outline"
-import {ChevronLeftIcon} from "@heroicons/vue/24/outline"
 import AppButton from "@/components/button/AppButton.vue"
 import OrderDetail from "@/app/order/OrderDetail.vue"
 import orderApi from "@/app/order/services/order.api"
+import OrderPrint from "@/app/order/OrderPrint.vue"
 import AppCard from "@/components/card/AppCard.vue"
 import OrderForm from "@/app/order/OrderForm.vue"
 import {ref} from "vue"
 
-const editMode = ref<boolean>(false)
+enum TabEnum {
+    Detail,
+    Edit,
+    Print
+}
+
+const tab = ref<TabEnum>(TabEnum.Detail)
 
 const props = defineProps<{
     order: OrderInterface
