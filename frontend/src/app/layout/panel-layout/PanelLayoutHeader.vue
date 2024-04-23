c02518842d3475176da8429cacc60a3021f334a3
Pouria <pnsadeghy@gmail.com>
2024-04-23 21:15:23 +0330

    Feat: Update Panel ui

---
+++ frontend/src/app/layout/panel-layout/PanelLayoutHeader.vue
@@ -0,0 +1,16 @@
+<template>
+  <app-panel-header @logout="appUtils.logout">
+      <template #logo>
+          <router-link :to="{name: DASHBOARD_PAGE}" class="flex items-center gap-x-2 text-xl font-bold" >
+              <ClipboardDocumentListIcon class="h-8 opacity-50" />
+              {{ $t("title") }}
+          </router-link>
+      </template>
+  </app-panel-header>
+</template>
+<script setup lang="ts">
+import AppPanelHeader from "@/components/panel-header/AppPanelHeader.vue"
+import {ClipboardDocumentListIcon} from "@heroicons/vue/24/outline"
+import {DASHBOARD_PAGE} from "@/router/names"
+import appUtils from "@/utils/app.utils"
+</script>
